76ad693ed6eb948703382e41234503ca1aec27ec
AleCarraDev <22fimdetarde@gmail.com>
2022-02-17 21:43:33 -0300

    estudo de caso + exercicios

---
+++ exercicio_semana09/exercicio-estudo-de-caso/src/components/Button/Button.js
@@ -0,0 +1,20 @@
+import "./Button.css";
+
+const Button = (props) => {
+  const { children, variant, isLoading, disabled, colorScheme, ...rest } =
+    props;
+
+  console.log(rest); // {onClick:function}
+
+  return (
+    <button
+      disabled={disabled || isLoading}
+      className={`button button__${variant}`}
+      {...rest} //onClick = {onClick}
+    >
+      {isLoading ? "carregando..." : children}
+    </button>
+  );
+};
+
+export default Button;
